a34f48f01eab1c4553164cf1ca1d4b175c169944
RamonCotarelo <ramoncotarelo99@gmail.com>
2021-10-14 21:34:04 +0200

    commit ordenador

--- arrays.php
+++ arrays.php
@@ -1,15 +1,15 @@
  
 <?php
 
-$tabla = array(
+$tablas = array(
     "fila 1" => array ("columna1.1","columna1.2", "columna1.3"),
     "fila 2" => array("columna2.1","columna2.2"),
     "fila 3" => array("columna3.1","columna3.2", "columna3.3"),
-    //"elemento1", "elemento2","elemento3","hola", "mundo","cada","dia", "es","peor"
+   
 
 );
 
-foreach($tabla as $clave => $array){
+foreach($tablas as $clave => $array){
 
         foreach($array as $columna){
             echo "$clave -->".$columna."</br>";
